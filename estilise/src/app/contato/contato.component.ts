21e6685fec6913b6a4634eba5545c68786fa52d8
Guilherme <gui.martinsa@gmail.com>
2020-05-24 22:50:34 -0300

    inclusão da página de contato.

--- estilise/src/app/contato/contato.component.ts
+++ estilise/src/app/contato/contato.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContatoComponent implements OnInit {
 
+  bloco1 = false;
+  bloco2 = true;
+  bloco3 = true;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  botao1 () {
+    this.bloco1 = !this.bloco1;
+    this.bloco2 = !this.bloco2;
+  }
 
+  botao2 () {
+    this.bloco2 = !this.bloco2;
+    this.bloco3 = !this.bloco3;
+  }
 }
