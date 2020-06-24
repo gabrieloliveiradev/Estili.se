16cd66799e9b58b78aff1037f24837a003bd027d
Guilherme <gui.martinsa@gmail.com>
2020-06-24 12:27:15 -0300

    correção model profissional

--- estilise/src/app/model/profissional.ts
+++ estilise/src/app/model/profissional.ts
@@ -1,13 +1,14 @@
 export class Profissional {
     public id_profissional:number
-    public nome:string
-    public senha:string
-    public emailusuario:string
-    public cartao_credito:string
-    public cep:string
-    public complemento:string
-    public cpf_usuario:string
-    public numero_casa:number
-    public telefone:number
-    public imagem_usuario:string
+    public nomeProfissional:string
+    public senha_profissional:string
+    public emailprofissional:string
+    public especialidade:string
+    public cep_profissional:string
+    public complemento_profissional:string
+    public cpf_cnpj:string
+    public numero_casa_profissional:number
+    public telefone_profissional:number
+    public imagem_profissional:string
+    public data_nascimento:string
 }
