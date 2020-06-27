c4db933f4cfb9428dddd1a74597675fa4634f4c3
Guilherme <gui.martinsa@gmail.com>
2020-06-27 15:37:39 -0300

    inicio da integração do front e back.

--- estilise/src/app/model/loginUsuario.ts
+++ estilise/src/app/model/loginUsuario.ts
@@ -1,5 +1,17 @@
 export class loginUsuario {
+    public id_usuario:number
+    public nome:string
     public senha:string
-    public token:string
     public emailusuario:string
+    public cartao_credito:string
+    public cep:string
+    public complemento:string
+    public cpf_usuario:string
+    public numero_casa:string
+    public telefone:string
+    public imagem_usuario:string
+    public data_nascimento:Date
+    public especialidade:string
+    public profissional : boolean
+    public token:string
 }
